8ac7b37d89e030fb88a9f2f3a45bb8ebcb06ce5d
villuette <udevrules@gmail.com>
2026-04-16 09:54:17 +0500

    nightly push

--- src/Inventory.gd
+++ src/Inventory.gd
@@ -8,6 +8,27 @@ var current_player: CharacterBody3D
 
 
 func _ready():
-	for i in range(total_slots):
-		var slot = slot_scene.instantiate()
-		grid.add_child(slot)
+    for i in range(total_slots):
+        var slot = slot_scene.instantiate()
+        grid.add_child(slot)
+
+
+func add_item(new_item: ItemData):
+    # 1. Если стакается, пробуем добавить в существующий стак
+    if new_item.stackable:
+        for slot in grid.get_children():
+            if slot.item_data and slot.item_data.title == new_item.title:
+                if slot.item_data.current_stack < slot.item_data.max_stack:
+                    slot.item_data.current_stack += 1
+                    slot.update_slot(slot.item_data)
+                    return true  # Успешно добавили
+
+    # 2. Ищем свободное место
+    for slot in grid.get_children():
+        if slot.item_data == null:
+            # Важно: делаем копию ресурса, чтобы не менять оригинал!
+            slot.update_slot(new_item.duplicate())
+            return true
+
+    print("Инвентарь полон!")
+    return false
